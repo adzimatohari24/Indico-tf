ad3f3538c218684479a5090c8967466520c62782
adzimatohari24 <adzimatohari24@gmail.com>
2026-06-05 11:33:28 +0700

    Update

--- tf-check.sh
+++ tf-check.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-# tf-check.sh - Automasi terraform init, fmt, validate, dan plan
+set -e
 
-RUN terraform init
-RUN terraform fmt
-RUN terraform validate
-RUN terraform plan
+echo "==> Terraform Init"
+terraform init
+
+echo "==> Terraform Format"
+terraform fmt -recursive
+
+echo "==> Terraform Validate"
+terraform validate
+
+echo "==> Terraform Plan"
+terraform plan
